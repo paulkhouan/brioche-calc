b2c19cab81909e7ad46e13d67c9558128efd8cf2
paulkhouan <99380502+paulkhouan@users.noreply.github.com>
2022-02-10 15:44:42 +0700

    Update brioche calc v1_1.py

--- brioche calc v1_1.py
+++ brioche calc v1_1.py
@@ -1,31 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# print('Bienvenue dans le calculateur de brioche. ')
-# qt_farine = float(input('Veuillez entrer la quantite de farine en grammes: '))
+print'Bienvenue dans le calculateur de brioche. ')
+qt_farine = float(input('Veuillez entrer la quantite de farine en grammes: '))
 
 # calcule des pourcentages des autres ingredient selon la recette suivante:
 # farine = 100, lait = 40, sucre = 16, sel = 2, oeuf = 0.4 (2 oeufs pour 500g de farine)
 
-# print('La quantite de lait necessaire est: ' + str(qt_farine * 40 / 100))
-# print('La quantite de sucre necessaire est: ' + str(qt_farine * 16 / 100))
-# print('La quantite de sel necessaire est: ' + str(qt_farine * 2 / 100))
-# print('La quantite en unite oeuf est: ' + str(qt_farine * 0.4 / 100))
-# print('La quantite de levure necessaire est: ' + str(qt_farine * 2 / 100))
+print('La quantite de lait necessaire est: ' + str(qt_farine * 40 / 100))
+print('La quantite de sucre necessaire est: ' + str(qt_farine * 16 / 100))
+print('La quantite de sel necessaire est: ' + str(qt_farine * 2 / 100))
+print('La quantite en unite oeuf est: ' + str(qt_farine * 0.4 / 100))
+print('La quantite de levure necessaire est: ' + str(qt_farine * 2 / 100))
 
 # version V1 successful.
 # prochaine etape, donner le choix de l'ingredient de base. Ex: si j'ai x quantite de lait...
